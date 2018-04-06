55d73d3424bb8facfacc42f06c8d1c2109d92e80
Shkelqim Behluli <shkelqimbehluli@gmail.com>
2018-04-06 17:10:43 +0200

    City Resources

--- app/src/main/java/android/shkelqimbehluli/com/weather/api/api.java
+++ app/src/main/java/android/shkelqimbehluli/com/weather/api/api.java
@@ -6,4 +6,7 @@ package android.shkelqimbehluli.com.weather.api;
 
 public class api {
 
+    public static final String API_Key="e7c75ebafb8910969cadcaaa39517929";
+    public static final String API_LINK="api.openweathermap.org/data/2.5/weather";
+
 }
