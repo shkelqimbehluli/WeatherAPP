5f0db67ba81e22c3b7046fbd5ee1efa9709aa363
Shkelqim Behluli <shkelqimbehluli@gmail.com>
2018-04-06 19:17:50 +0200

    Main Model

--- app/src/main/java/android/shkelqimbehluli/com/weather/models/Weather.java
+++ app/src/main/java/android/shkelqimbehluli/com/weather/models/Weather.java
@@ -7,6 +7,13 @@ public class Weather {
     private String description;
     private String icon;
 
+    public Weather(int id, String main, String description, String icon) {
+        this.id = id;
+        this.main = main;
+        this.description = description;
+        this.icon = icon;
+    }
+
     public int getId() {
         return id;
     }
