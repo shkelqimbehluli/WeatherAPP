64c5156b7b3122a9abf1075165a4d5c79a50e7de
Shkelqim Behluli <shkelqimbehluli@gmail.com>
2018-04-06 19:10:55 +0200

    Coordinate Model

---
+++ app/src/main/java/android/shkelqimbehluli/com/weather/models/Coordinate.java
@@ -0,0 +1,28 @@
+package android.shkelqimbehluli.com.weather.models;
+
+public class Coordinate {
+
+    public double lat;
+    public double lon;
+
+    public Coordinate(double lat, double lon){
+        this.lat = lat;
+        this.lon = lon;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public double getLon() {
+        return lon;
+    }
+
+    public void setLon(double lon) {
+        this.lon = lon;
+    }
+}
